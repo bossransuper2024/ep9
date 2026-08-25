4eead0e5a3895a0ac1e6cb0a3c8fe0947b9c23e2
EP9 Deploy <ep9-bot@local>
2026-08-25 08:25:02 +0800

    Build: update dist for gh-pages deployment

---
+++ assets/js/downloads.js
@@ -0,0 +1,26 @@
+window.RGSE_DOWNLOAD_MIRRORS = {
+  googleDrive: [
+    {
+      label: "Google Drive English Language",
+      url: "https://drive.google.com/file/d/1umtckbzvUtoaV35ZgKHmkxM_Nj2y7WHS/view?usp=sharing",
+      note: "English full client",
+    },
+    {
+      label: "Google Drive Chinese Language",
+      url: "https://drive.google.com/file/d/1MBZwUIqPDuqGw52BMw4VOvRT5ZlqZfdM/view?usp=sharing",
+      note: "Chinese full client",
+    },
+  ],
+  mediafire: [
+    {
+      label: "MediaFire English Language",
+      url: "https://www.mediafire.com/file/pkvy337fjxrv3pd/Ran+GS+Eternity+EN.exe/file",
+      note: "English full client",
+    },
+    {
+      label: "MediaFire Chinese Language",
+      url: "https://www.mediafire.com/file/xlnc58fj0lmuqjb/Ran+GS+Eternity+TW.exe/file",
+      note: "Chinese full client",
+    },
+  ],
+};
